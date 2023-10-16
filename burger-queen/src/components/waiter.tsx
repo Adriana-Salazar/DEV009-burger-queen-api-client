64afc6a9e4f4658b15f1d5df3b4505eaf4f125df
ingrid1802 <ingridaybar841@gmail.com>
2023-10-16 10:46:28 -0500

    peticion data ordenes y agregamos boton de bebidas en la vista de para hacer ordenes

--- burger-queen/src/components/waiter.tsx
+++ burger-queen/src/components/waiter.tsx
@@ -1,16 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
+interface Order {
+  id: number;
+  client: string;
+  dataEntry: string;
+  status: string;
+  dateProcessed?: string | null;
+}
+
+interface WaiterProps {
+  token: string; // Asegúrate de recibir el token como prop
+}
+
+export function Waiter({ token }: WaiterProps) {
+  const [orders, setOrders] = useState<Order[]>([]);
+
+  useEffect(() => {
+    async function fetchOrders() {
+      try {
+        const response = await fetch('https://burger-queen-mock-9l2y.onrender.com/orders', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('La solicitud no fue exitosa');
+        }
+        const data = await response.json();
+        setOrders(data);
+      } catch (error) {
+        console.error('Error al cargar las órdenes:', error);
+      }
+    }
+
+    fetchOrders();
+  }, [token]);
+  return (
+    <TablaOrdenMesero orders={orders} />
+  );
+}
 
 
-
-export function TablaOrdenMesero() {
+export function TablaOrdenMesero({ orders }: { orders: Order[] }) {
+  console.log("Datos de órdenes:", orders);
   return (
     <>
       <div className='table-responsive'>
-        <table className=" estilos table table-striped mx-auto">
+        <table className="estilos table table-striped mx-auto">
           <thead>
             <tr>
               <th className='style text-center'>N° Orden</th>
@@ -21,19 +61,31 @@ export function TablaOrdenMesero() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th className='style2 text-center'>001</th>
-              <td className='style2 text-center'>Jorge Mendoza</td>
-              <td className='style2 text-center'>7:10am</td>
-              <td className='style2 text-center'>Listo</td>
-              <td className='style2 text-center'>7:20am</td>
-              <td className='style2 text-center'><FontAwesomeIcon icon={faTrash} style={{ color: "#000000" }} /></td>
-            </tr>
+            {orders ? (
+              orders.map((order) => (
+                <tr key={order.id}>
+                  <td className='style2 text-center'>{order.id}</td>
+                  <td className='style2 text-center'>{order.client}</td>
+                  <td className='style2 text-center'>{order.dataEntry}</td>
+                  <td className='style2 text-center'>{order.status}</td>
+                  <td className='style2 text-center'>
+                    {order.dateProcessed || "Aún no entregado"}
+                  </td>
+                  <td className='style2 text-center'>
+                    <FontAwesomeIcon icon={faTrash} style={{ color: "#000000" }} />
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan={5}>Cargando órdenes...</td>
+              </tr>
+            )}
           </tbody>
         </table>
       </div>
     </>
-  )
+  );
 }
 
 
